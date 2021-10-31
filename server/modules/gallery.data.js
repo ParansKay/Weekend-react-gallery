cc216dd85fc91f073d445116eb823dc0eedc83b5
Paran Kashani <parankashani@gmail.com>
2021-10-30 19:12:43 -0500

    like and unlike button work

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,9 @@
 const galleryItems = [
-    { id: 1, path: './images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: './images/PP2.png', description: 'Me and Penelope!', likes: 0 },
     { id: 2, path: './images/Lumi_nose.png', description: '"Got your nose!"', likes: 0 },
     { id: 3, path: './images/Lumi!.png', description: 'Lumi!', likes: 0 },
     { id: 4, path: './images/PD.png', description: 'Thanksgiving', likes: 0 },
-    { id: 5, path: './images/PP1.png', description: 'Me and floof!', likes: 0 }
+    { id: 5, path: './images/PP1.png', description: 'Floof is not having it', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,20 +2,77 @@ import { useState } from "react";
 import axios from "axios";
 
 function GalleryItem( props ){
-    // const[name, setName]= useState( null );
+
+    // create a variable named showImage and set it's value to true
     const [showImage, setShowImage] = useState (true);
+    // create a function called toggleShowImage
+    const toggleShowImage = () => {
+        // the function sets the value of showImage to not-showImage (changes it from true to false)
+        setShowImage(!showImage);
+      } //end toggleShowImage
 
+    //create a variable called likeUnlike and set it's value to false (because pictures are unliked in their default state)
+    const [likeUnlike, setLikeUnlike ] = useState (false);
+    //create a function called toggleLikeUnlike (we're going to create a button for it in the return div)
+    const toggleLikeUnlike =()=>{
+        // if something is not liked, 
+        if( !likeUnlike ){   
+            // set the value of likeUnlike to true (heart the image)
+            setLikeUnlike(true)
+            addLike();
+        }
+        // otherwise, if the image is liked, set the value to false (un-heart)
+        else{
+            setLikeUnlike(false)
+            removeLike();
+        }
+    }// end toggleLikeUnlike
 
-    const toggleShow = () => {
-        setShow(!show);
-      } //end toggleShow
+    const addLike=()=>{
+        axios.put( `/gallery/addLike/${props.imageToSendtoGI.id}`, props.imageToSendtoGI ).then( (response)=>{
+        props.getImages();
+        }).catch((err)=>{
+        console.log('error:', err);
+        });
+    }//end addLike
+
+    const removeLike=()=>{
+        axios.put( `/gallery/removeLike/${props.imageToSendtoGI.id}`, props.imageToSendtoGI ).then( (response)=>{
+        props.getImages();
+        }).catch((err)=>{
+        console.log('error:', err);
+        });
+    }
 
     return(
         <div>
-            {/* {
-            show? */}
-            
-            <img src={ props.imageToSendtoGI.path }/>
+            {/* Calling our toggle function and defining what happens when it's used */}
+            {
+            // if showImage is true, display the image
+            showImage?
+            // if yes: 
+            <img onClick={toggleShowImage} src={ props.imageToSendtoGI.path }/>:
+            // if no:
+            <h2 onClick={toggleShowImage}> {props.imageToSendtoGI.description}</h2>
+            }
+
+            {/* Calling our toggleLikeUnlike function and defining what happens when it's used */}
+            {
+            // if like is true,
+            likeUnlike?
+            // then, we can un-heart the image
+            <button onClick={toggleLikeUnlike}>un-Heart</button> :
+            // else, if it's false, we can heart it
+            <button onClick={toggleLikeUnlike}>Heart</button> 
+              }
+
+            { 
+             //if someone has liked it, display happy message with count, otherwise, sad 'no people' message
+            // numLikes > 0 ?
+            // <h3>{numLikes} people love this!</h3> :
+            // <h3>No people love this :(</h3>
+            }
+
         </div>
     )
 }
